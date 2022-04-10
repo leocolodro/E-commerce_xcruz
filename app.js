0c1ee4f8237ae80debe9158d2f74f86ec3b91246
Bautista <bautivicens@gmail.com>
2022-04-10 18:23:39 -0300

    First commit (develop)

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Express server port.
+const port = 3030;
+
+app.listen(port, () => {
+    console.log("Server status: Online");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/home.html"))
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/home.html"))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/register.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/login.html"))
+})
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/cart.html"))
+})
+
+app.get('/product_detail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/productDetail.html"))
+})
