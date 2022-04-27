1124653c4b172e55ad65e225e0c1414ac535c165
leocolodro <leocolodro@gmail.com>
2022-04-26 23:10:10 -0300

    last commit feature-home

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 app.use(express.static('public'));
+const path = require('path')
 
-//Express server port.
-const port = 3030;
-
-app.listen(port, () => {
-    console.log("Server status: Online");
+app.listen(3031, ()=>{
+    console.log('Servidor funcionando');
 });
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "/views/home.html"))
-// });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/views/home.html"))
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/views/register.html"))
-})
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/views/login.html"))
-})
-
-app.get('/cart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/views/cart.html"))
-})
-
-app.get('/product_detail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/views/productDetail.html"))
-})
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
