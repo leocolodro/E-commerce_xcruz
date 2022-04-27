6b5991d5fe1feab52a760fcc7ec0774a0cd6dcad
leocolodro <leocolodro@gmail.com>
2022-04-26 23:20:09 -0300

    last last commit feature-home

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
 const path = require('path')
 
-app.listen(3031, ()=>{
+app.listen(3030, ()=>{
     console.log('Servidor funcionando');
 });
+
+app.use(express.static('public'));
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
