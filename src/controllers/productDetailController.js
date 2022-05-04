760c0d6f30aa387217aa64d6c3d18cc32a95e5c5
Bautista <bautivicens@gmail.com>
2022-05-04 02:25:17 -0300

    productDetail dynamic products showcase partialy implemented

--- src/controllers/productDetailController.js
+++ src/controllers/productDetailController.js
@@ -1,15 +1,24 @@
+//Path Module.
 const path = require('path');
+
+//products-array Module.
+/*(!)Por el momento se le hara mención en comentarios semejandolo con una base de datos*/ 
+/*(!) Quitar, a futuro, por la database de los productos*/ 
 const productos = require('../products-array');
 
 const ProductDetailController = {
+    
+    //Mostrar el detalle del producto
     display: function(req, res){
+
+        //Buscar  en la base la id del producto pasado por paramentros en el req.
         const producto = productos.find(producto => {
             return producto.id == req.params.id;
         });
 
         /*Si no encuentra el producto*/
         if(producto == undefined){
-            res.send(console.log("Plato no encontrado."));
+            res.send("ERROR.\nProducto no encontrado!");
         }
 
         /*Sí encuentra el producto*/
@@ -19,4 +28,5 @@ const ProductDetailController = {
     }
 }
 
+//Export.
 module.exports = ProductDetailController;
--- src/products-array/index.js
+++ src/products-array/index.js
@@ -1,5 +1,9 @@
 //Este modulo es a modo demostrativo.
 //Los datos dentro de este deberan ser colocados en una base de datos.
+//Author: Bautista.
+
+//Path Module.
+const path = require('path');
 
 const productos = [
     {
@@ -9,6 +13,8 @@ const productos = [
         categoria: "Botas",
         porcentajeDescuento: "0",
         precio: "10500",
+        talles: [38, 40, 42, 43, 44],
+        imagenesUrl: ['../images/zapatos/producto_1/zapato_cuero_marron_1.jpg','../images/zapatos/producto_1/zapato_cuero_marron_2.jpg','../images/zapatos/producto_1/zapato_cuero_marron_3.jpg']
         
     },
     {
@@ -18,6 +24,8 @@ const productos = [
         categoria: "De vestir",
         porcentajeDescuento: "0",
         precio: "12990",
+        talles: [39, 41, 42],
+        imagenesUrl: ['../images/zapatos/producto_2/zapato_cuero_punta_negro_1.jpg', '../images/zapatos/producto_2/zapato_cuero_punta_negro_2.jpg', '../images/zapatos/producto_2/zapato_cuero_punta_negro_3.jpg']
     },
     {
         id: "3",
@@ -25,6 +33,8 @@ const productos = [
         colores: ["Marrón"],
         porcentajeDescuento: "0",
         precio: "10500",
+        talles: [40, 42],
+        imagenesUrl: ['../images/zapatos/producto_3/bota_chelsea_cuero_negro_1.jpg','../images/zapatos/producto_3/bota_chelsea_cuero_negro_2.jpg','../images/zapatos/producto_3/bota_chelsea_cuero_negro_3.jpg', '../images/zapatos/producto_3/bota_chelsea_cuero_negro_4.jpg']
     }
 ];
 
