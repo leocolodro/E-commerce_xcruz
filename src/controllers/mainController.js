367483378a070942ef3e7460782df97b96bdd09e
Bautista <bautivicens@gmail.com>
2022-05-16 01:59:47 -0300

    <a> added to productos, random products sort for 'Productos destacados' section

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,12 +8,30 @@ const path = require('path');
 const productosFilePath = path.join(__dirname, '../data/products-database.json');
 
 //Productos DataBase
-const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
+let productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
 
 const MainController = {
     displayHome: function(req, res){
-        res.render(path.join(__dirname, '../views/home.ejs'), {productos: productos});
-    },
+        //Tamaño minimo array para aleatoriedad
+        const minArrayLength = 4; 
+        let productosDestacadosArray = [];
+
+        //Si el tamaño del array "productos" es inferior al valor de la constante "minArraylenght" 
+        if(productos.length < minArrayLength){
+            productosDestacadosArray = productos;
+        }
+        //Si es el tamaño del array: "productos" es mayor al valor de la constante "minArraylenght" 
+        else{
+            //Sortear de manera aleatoria entre 4 items del array: "productos"
+            productos.sort(() => Math.random() > 0.5 ? 1 : -1);
+            
+            //Tomar 4 productos del array productos.
+            productosDestacadosArray = productos.slice(0,4);
+
+            res.render(path.join(__dirname, '../views/home.ejs'), {productos: productosDestacadosArray});
+        }
+    }
 }
+
 
 module.exports = MainController;
