12d239d376aae2a7215740cfd81ad5d98f6031c5
Bautista <bautivicens@gmail.com>
2022-05-15 23:43:31 -0300

    First commit reingenieria-home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,24 +1,19 @@
+//File System Module.
+const fs = require('fs');
+
+//Path Module.
 const path = require('path');
+
+//Path de la base de datos de los productos.
+const productosFilePath = path.join(__dirname, '../data/products-database.json');
+
+//Productos DataBase
+const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
 
 const MainController = {
     displayHome: function(req, res){
-        res.render(path.join(__dirname, '../views/home.ejs'));
-    } ,
-
-    /*detalleMenu: function(req, res){
-        
-        let plato = menu.find(element =>{
-                               //Obtenemos la id del url del parametro del req
-            return element.id ==  req.params.id;
-        });
-
-        if(plato == undefined){
-            res.send(console.log("Plato no encontrado."));
-        }
-        else{
-            res.render(path.join(__dirname, '../views/detalleMenu.ejs'), {platillo: plato});
-        }
-    }*/
+        res.render(path.join(__dirname, '../views/home.ejs'), {productos: productos});
+    },
 }
 
 module.exports = MainController;
