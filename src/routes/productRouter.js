be3f48131500a38c216ac47c70734371ace874ac
leocolodro <leocolodro@gmail.com>
2022-05-18 17:55:40 -0300

    last commit feature-routes-controllers

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const ProductController = require('../controllers/productController.js');
 
+router.get('/', ProductController.listadoDeProductos)
+
 router.get('/:id', ProductController.display);
+
+
+router.post('/nuevo', ProductController.guardarProducto)
+
+router.put('/:id/editar', ProductController.editar);
+
+router.delete('/:id/borrar', ProductController.borrar)
 
 module.exports = router;
