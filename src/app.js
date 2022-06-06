c4d13da5ee7c7e4251a6a6ecd386d640faddacf8
Bautista <bautivicens@gmail.com>
2022-06-05 23:43:45 -0300

    bug fix

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.set("view engine", "ejs");
 const mainRouter = require(path.join(__dirname, '/routes/mainRouter.js'));
 
 app.listen(process.env.PORT || port, ()=>{
-    console.log("Server Status: Online");
+    console.log("Server Status: Online\nUrl: localhost:3030/");
 });
 
 //Main Router - invocation
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -6,16 +6,16 @@ const router = express.Router();
 const path = require('path');
 
 //MainController - require
-const MainController = require(path.join(__dirname, '../controllers/mainController.js'));
+const mainController = require(path.join(__dirname, '../controllers/mainController.js'));
 
 /*+++++++++++++++ Routers - require´s ++++++++++++++++++*/
 const productRouter = require(path.join(__dirname, '../routes/productRouter.js'));
 const userRouter = require(path.join(__dirname,'../routes/userRouter.js'));
 const cartRouter = require(path.join(__dirname,'../routes/cartRouter.js'));
 
-router.get('/', MainController.displayHome);
+router.get('/', mainController.displayHome);
 
-router.use('/user', userRouter);
+router.use('/usuario', userRouter);
 
 router.use('/producto', productRouter);
 
